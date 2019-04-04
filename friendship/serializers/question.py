2753e53f07fd0901e2a1274fc148b83b77ad54c2
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-04-04 17:32:28 +0800

    PEP8, bandit, isort for serializer classes

--- friendship/serializers/question.py
+++ friendship/serializers/question.py
@@ -1,15 +1,17 @@
 from rest_framework import serializers
-from .answer import AnswerSerializer
+
 from ..models import Question
+from .answer import AnswerSerializer
+
 
 class QuestionSerializer(serializers.ModelSerializer):
-  """
-  Question model serializer. It will include answer choice in response too
-  through nested serializer `AnswerSerializer`
-  """
-  class Meta:
-    model = Question
-    fields = '__all__'
-    depth = 1
+    """
+    Question model serializer. It will include answer choice in response too
+    through nested serializer `AnswerSerializer`
+    """
+    class Meta:
+        model = Question
+        fields = '__all__'
+        depth = 1
 
-  answers = AnswerSerializer(many=True, source='question_answers')
+    answers = AnswerSerializer(many=True, source='question_answers')
