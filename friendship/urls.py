259e5c868a5b6dc1104fc5c7fc44f67683945b64
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-04-04 18:22:03 +0800

    quiz details url rule

--- friendship/urls.py
+++ friendship/urls.py
@@ -2,10 +2,11 @@ from rest_framework import routers
 from rest_framework_nested import routers as nested_routers
 from django.conf.urls import url
 from django.urls import include
-from .views import QuestionViewSet, QuizViewSet
+from .views import QuestionViewSet, QuizViewSet, QuizDetailsViewSet
 
 router = routers.DefaultRouter()
 router.register(r'quiz', QuizViewSet)
+router.register(r'quiz_details', QuizDetailsViewSet)
 router.register(r'question', QuestionViewSet, base_name='Question')
 
 # Nested Router for /quiz/<pk>/questions/ endpoint.
